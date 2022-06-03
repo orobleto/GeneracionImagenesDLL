7ba8da0afdce1f9e7ef709ebad02477fb93f7248
Octavio Robleto <octavio.robleto@gmail.com>
2022-06-03 16:40:48 -0300

    Creacion de Log

---
+++ Prueba_SGFCON_V1/Prueba_SGFCON_V1/Program.cs
@@ -0,0 +1,16 @@
+﻿using SGFCON_V1;
+using System;
+
+namespace Prueba_SGFCON_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImagenesSignafile imagenesSignafile = new ImagenesSignafile();
+            long imagenes = imagenesSignafile.sgfcongf("000 0 1000 00000125882 FERNANDO 123456,78 15000010024554014200013202031 TranID0123");
+            Console.WriteLine(imagenes);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SGFCON_V1/SGFCON_V1/ImagenesSignafile.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SGFCON_V1
+{
+    public class ImagenesSignafile
+    {
+
+        public long sgfcongf(string Params)
+        {
+
+            string pathArchivo = @"images";
+            string pathImagenes = @"ImagenesPrueba";
+            Random random = new Random();
+            int aleatorio = random.Next(0, 4);
+
+
+            // borro las imagenes
+            for (int i = 0; i < 3; i++)
+            {
+                String nombre = "0"+ (i + 1) + ".BMP";
+                if (File.Exists(Path.Combine(pathArchivo, nombre)))
+                {
+                    File.Delete(Path.Combine(pathArchivo, nombre));
+                }
+            }
+
+            // copio las imagenes
+            for (int i = 0; i < aleatorio; i++)
+            {
+                String nombre = "0" + (i + 1) + ".BMP";
+                File.Copy(Path.Combine(pathImagenes, "firma.bmp"), Path.Combine(pathArchivo, nombre), true);
+            }
+
+            try
+            {
+                // creo el archivo si existe
+                using (FileStream fs = File.Create(Path.Combine(pathArchivo, "sgfcongf_log.txt")))
+                {
+                    string[] arreglo = Params.Split(' ');
+                    Params = "";
+                    for (int i = 0; i < arreglo.Length; i++)
+                    {
+                        Params += "P" + (i + 1) + " = \"" +arreglo[i]+"\"\n";
+                    }
+
+                    byte[] info = new UTF8Encoding(true).GetBytes(Params + "\nImagenes Generadas: " + aleatorio);
+                    //agrego la info
+                    fs.Write(info, 0, info.Length);
+
+                    
+                }
+
+                // abro el flujo y leo.
+              /*  using (StreamReader sr = File.OpenText(Path.Combine(pathArchivo, "sgfcongf.txt")))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+              */
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return aleatorio;
+        }
+
+    }
+}
